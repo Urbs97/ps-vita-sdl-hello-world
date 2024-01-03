bfee61aae721ce91656781f50654f59e41f3f13d
Urbs97 <urbs97@pm.me>
2024-01-03 21:29:43 +0100

    Rendering sprite and reading from controller

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # ps-vita-sdl-hello-world
+This project has been tested on real hardware (firmware 3.60).
+
+## How to Build
+Install [VITASDK](https://github.com/vitasdk/vdpm) (more information [here](https://vitasdk.org/)).  
+To build:
+```
+cmake .
+make
+```
+
+## How to get includes in VSCode
+Set the following lines in the include path:
+```
+${workspaceFolder}/**
+${VITASDK}/include
+${VITASDK}/arm-vita-eabi/include
+```
+
+## VitaSdk Documentation
+https://docs.vitasdk.org/
+
+## SDL2
+https://wiki.libsdl.org/SDL2/README/vita  
+https://github.com/libsdl-org/SDL/tree/SDL2
+
+## Safe Time with Vita3k
+Instead of always pushing and installing the .vpk to your Vita you can directly run it inside the Vita3k emulator. You can install it from [here](https://github.com/Vita3K/Vita3K) and run the command `make run`. This assumes you have the Vita3k AppImage installed or linked into the workspace and named `Vita3K-x86_64.AppImage`. You can adjust this behavior inside the `CMakeLists.txt`.
---
+++ src/main.cpp
@@ -0,0 +1,84 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <psp2/kernel/processmgr.h>
+
+// Screen dimension constants
+enum
+{
+    SCREEN_WIDTH = 960,
+    SCREEN_HEIGHT = 544
+};
+
+SDL_Texture* LoadSprite(const char* file, SDL_Renderer* renderer)
+{
+    SDL_Texture* texture = IMG_LoadTexture(renderer, file);
+    if (texture == nullptr)
+    {
+        return nullptr;
+    }
+    return texture;
+}
+
+void RenderSprite(SDL_Texture* sprite, SDL_Renderer* renderer, int x, int y)
+{
+    SDL_Rect dest;
+    dest.x = x;
+    dest.y = y;
+    SDL_QueryTexture(sprite, NULL, NULL, &dest.w, &dest.h);
+    SDL_RenderCopy(renderer, sprite, NULL, &dest);
+}
+
+auto main() -> int
+{
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER))
+        return -1;
+
+    SDL_Window* window = SDL_CreateWindow("window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 480, 272, 0);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    SDL_Texture* sprite = LoadSprite("sprites/sprite.png", renderer);
+
+    SDL_Event event;
+    bool renderSprite = false;
+    bool running = true;
+    while (running)
+    {
+        if (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                running = false;
+                break;
+            case SDL_CONTROLLERDEVICEADDED:
+                SDL_GameControllerOpen(event.cdevice.which);
+                break;
+            case SDL_CONTROLLERBUTTONDOWN:
+                if (event.cbutton.button == SDL_CONTROLLER_BUTTON_START)
+                    running = false;
+                if (event.cbutton.button == SDL_CONTROLLER_BUTTON_A)
+                    renderSprite = true;
+                break;
+            }
+        }
+
+        // Clear the screen
+        SDL_RenderClear(renderer);
+
+        // Draw everything on a dark blue background
+        SDL_SetRenderDrawColor(renderer, 0, 0, 128, 255);
+
+        // Draw sprite
+        if (renderSprite && sprite != nullptr)
+        {
+            RenderSprite(sprite, renderer, 100, 100); // Render at position 100, 100
+        }
+
+        // Present changes
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_Quit();
+    return 0;
+}
