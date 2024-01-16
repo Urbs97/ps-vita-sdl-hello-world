952e10561e74be1951f7dfa6289ee39c03c60f82
Urbs97 <urbs97@pm.me>
2024-01-16 21:10:10 +0100

    Imgui with SDL2 backend

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_sdl2.h>
+#include <imgui/imgui_impl_sdlrenderer2.h>
 #include <psp2/kernel/processmgr.h>
 
 // Screen dimension constants
@@ -36,6 +39,11 @@ auto main() -> int
 
     SDL_Texture* sprite = LoadSprite("sprites/sprite.png", renderer);
 
+    // Initialize ImGui
+    ImGui::CreateContext();
+    ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer2_Init(renderer);
+
     SDL_Event event;
     bool renderSprite = false;
     bool running = true;
@@ -43,6 +51,8 @@ auto main() -> int
     {
         if (SDL_PollEvent(&event))
         {
+            // ImGui SDL input
+            ImGui_ImplSDL2_ProcessEvent(&event);
             switch (event.type)
             {
             case SDL_QUIT:
@@ -55,7 +65,7 @@ auto main() -> int
                 if (event.cbutton.button == SDL_CONTROLLER_BUTTON_START)
                     running = false;
                 if (event.cbutton.button == SDL_CONTROLLER_BUTTON_A)
-                    renderSprite = true;
+                    renderSprite = !renderSprite;
                 break;
             }
         }
@@ -66,16 +76,37 @@ auto main() -> int
         // Draw everything on a dark blue background
         SDL_SetRenderDrawColor(renderer, 0, 0, 128, 255);
 
-        // Draw sprite
-        if (renderSprite && sprite != nullptr)
+        if (renderSprite)
+            RenderSprite(sprite, renderer, 30, 30);
+
+        ImGui_ImplSDLRenderer2_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::ShowDemoWindow();
+
+        if (ImGui::Begin("Spawn enemies"))
         {
-            RenderSprite(sprite, renderer, 100, 100); // Render at position 100, 100
+            ImGui::Text("Press X to spawn a sprite");
+            if (ImGui::Button("Or press this button"))
+                renderSprite = !renderSprite;
+            ImGui::Text("Press START to exit");
         }
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
 
         // Present changes
         SDL_RenderPresent(renderer);
     }
 
+    ImGui_ImplSDLRenderer2_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
